4c73e9eb1a45f78d396a061b454b665b3bba2d77
Sherlock <zcesfzh@ucl.ac.uk>
2024-01-15 03:07:25 +0000

    zxc

--- flask_app1/hello.py
+++ flask_app1/hello.py
@@ -1,10 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Hello</title>
-</head>
-<body>
-    <p>Hello, World!</p>
-</body>
-</html>
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello</title>
+    </head>
+    <body>
+        <p>Hello, World!</p>
+    </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
